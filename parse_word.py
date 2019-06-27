df2e14a56eccdf278e992aa2c82cfc2a384f6582
Wei Hainan <weihainan@kuandeng.com>
2019-06-27 14:43:09 +0800

    格式化代码

--- parse_word.py
+++ parse_word.py
@@ -3,6 +3,7 @@
 # 从本地百词斩单词中解析并输出到一个文件中，方便背诵
 import os, json, codecs
 
+
 def parse_single_word(word):
     jsonData = json.loads(word)
     word = jsonData["word"]
@@ -17,16 +18,19 @@ def parse_single_word(word):
     sentence_trans = jsonData["sentence_trans"]
     return "%s;%s;%s;%s;%s,%s\n" % (word, accent, mean_cn, mean_en, sentence, sentence_trans)
 
+
 def parse_word(path):
     with codecs.open("all_words.txt", "a+", "utf-8-sig") as fp_w:
         for dirpath, dirnames, filenames in os.walk(path):
             for file in filenames:
                 fullpath = os.path.join(dirpath, file)
-               # print fullpath
-                if fullpath.find("meta.json")!=-1:
+                # print fullpath
+                if fullpath.find("meta.json") != -1:
                     fp = open(fullpath)
                     word = fp.readline()
                     fp_w.write(parse_single_word(word))
 
+
 if __name__ == '__main__':
     parse_word("/Users/weihainan/Documents/github/baicizhan_zpk/extra_zpk")
+
